bfe2996b0e7e2debfb715cb0a888254f09ab29cc
Remi VALTON <rvalton@e2r11p7.clusters.42paris.fr>
2022-04-05 18:08:18 +0200

    test modif

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 18:13:33 by melperri          #+#    #+#             */
-/*   Updated: 2022/04/04 18:13:46 by melperri         ###   ########.fr       */
+/*   Updated: 2022/04/05 18:08:00 by rvalton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "SimpleSocket.hpp"
 
 int	main() {
+	std::cout << "modif";
 	return 0;
 }
