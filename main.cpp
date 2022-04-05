6860957ff0c45fa806448603e8a46487d09dad96
rvalton <rvalton@student.42.fr>
2022-04-05 18:19:27 +0200

    last modif

--- main.cpp
+++ main.cpp
@@ -6,14 +6,12 @@
 /*   By: melperri <melperri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 18:13:33 by melperri          #+#    #+#             */
-/*   Updated: 2022/04/05 18:12:51 by rvalton          ###   ########.fr       */
+/*   Updated: 2022/04/05 18:19:15 by rvalton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "SimpleSocket.hpp"
-#include <iostream>
 
 int	main() {
-	std::cout << "modif";
 	return 0;
 }
