e80c2efd69d30f646f4eafe8957fd7c85fd5736e
rvalton <rvalton@student.42.fr>
2022-04-17 04:40:56 +0200

    pas mal

--- socket_httpResponse.cpp
+++ socket_httpResponse.cpp
@@ -27,17 +27,11 @@ int	main(void) {
 		close(server_fd);
 		exit(-1);
 	}
-	/*	if (fcntl(server_fd, F_SETFL, O_NONBLOCK) == -1) {
-		perror("fcntl failed");
-		close(server_fd);
-		exit(-1);
-		}*/
 	struct sockaddr_in	address;
 	memset((char *)&address, 0, sizeof(address));
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
-	//	memset(address.sin_zero, '\0', sizeof(address.sin_zero));
 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
 		perror("bind failed");
 		return 0;
@@ -50,7 +44,6 @@ int	main(void) {
 	int	addrlen = sizeof(address);
 	long	valread;
 	while (1) {
-		std::string	get_http = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
 		std::string	str_file = "";
 		if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
 						(socklen_t *)&addrlen)) < 0) {
@@ -67,25 +60,17 @@ int	main(void) {
 			}
 		}
 		if (request == "GET /home.html HTTP/1.1\r") {
-			std::ifstream	fichier_in("home.html");
-			str_file = std::string(
-					std::istreambuf_iterator<char>(fichier_in),
-					std::istreambuf_iterator<char>());
+			HttpResponse	msg;
+			str_file = msg.getHttpResponse("home.html");
+			std::cout << str_file << "\n";
 		} else {
-			str_file = "Hello World!";
+			HttpResponse	msg;
+			str_file = msg.getHttpResponse("not_found.html");
 		}
 
-		std::stringstream	len_content;
-		len_content << str_file.size();
-		std::string	read_len = len_content.str();;
-
-		get_http += read_len;
-		get_http += "\n\n";
-		get_http += str_file;
-		std::cout << get_http << "\n";
 		std::cout << new_socket << "\n";
-		if (send(new_socket, get_http.c_str(), get_http.size(),
-					0) == get_http.size())
+		if (send(new_socket, str_file.c_str(), str_file.size(),
+					0) == str_file.size())
 			close(new_socket);
 	}
 	return 0;
